3df87e429a8accc73e152488d25d8d38c9d5ed10
James Ahlborn <jtahlborn@yahoo.com>
2023-08-25 21:23:43 -0400

    cleanup script slightly

--- connect_sync.legacy.sh
+++ connect_sync.legacy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# information to populate
+
+set -e
+
+### information to populate
+###
 WYZE_EMAIL=youremail
 WYZE_PASSWORD=yourpassword
 WYZE_KEY_ID=your_ID
@@ -12,20 +16,22 @@ export WYZE_PASSWORD
 export WYZE_KEY_ID
 export WYZE_API_KEY
 cd ~/path_to_your_script/
-# end of information to populate
-export PATH="$HOME/.local/bin:$PATH"
+###
+### end of information to populate
+
 python3 ./scale.py
+
 if md5sum -c ./cksum.txt; then
-echo "no new measurment"
-exit 0
+  echo "no new measurment"
+  exit 0
 else
-if gupload -u $Garmin_username -p $Garmin_password  -v 1 ./wyze_scale.fit; then
-echo "file uploaded"
-md5sum ./wyze_scale.fit > ./cksum.txt
-exit 0
-else
-echo "file not uploaded"
-fi
+  if gupload -u $Garmin_username -p $Garmin_password  -v 1 ./wyze_scale.fit; then
+    echo "file uploaded"
+    md5sum ./wyze_scale.fit > ./cksum.txt
+    exit 0
+  else
+    echo "file not uploaded"
+  fi
 fi
 exit 0
 
